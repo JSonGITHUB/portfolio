5c4a1a7728aec5981d460ade899f5d60594642f3
JSonGITHUB <jhnsntcc@gmail.com>
2018-04-19 14:02:10 -0700

    Ad Videos width 350px

--- js/Advertising.js
+++ js/Advertising.js
@@ -24,7 +24,7 @@ function showAdvertising() {
 
             document.getElementById("resume").innerHTML = document.getElementById("resume").innerHTML +
 
-            "<video class='ad' width='400'controls>" +
+            "<video class='ad' controls>" +
                 "<source src='assets/banners/" + vids[x] + "' type='video/mp4'>" +
                 "Your browser does not support HTML5 video." +
             "</video>";
