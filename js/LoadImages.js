eeafa35b496d6ffe086eb844da9081c240124eef
JSonGITHUB <jhnsntcc@gmail.com>
2018-05-23 13:19:39 -0700

    LoadImages optimized

--- js/LoadImages.js
+++ js/LoadImages.js
@@ -1,42 +1,21 @@
-//interactive.forEach((img, i) => (img))
-
-function getImages(css, path, imgArray, index) {
+let getImages = (css, path, imgArray, index) => {
     clearContent();
     getImage(css, path, imgArray, index);
-}
-
-async function getImage(css, path, imgArray, index) {
-
-    let image = await loadImage(css, path, imgArray, index);
-    let load = await addImg(css, image.src);
-    
-}
-
-function loadImage(css, path, imgArray, index) {
-
-    let url = path + imgArray[index].image;
-
-    return new Promise((resolve,reject) => {
-        let image = new Image();
-        image.onload = () => { 
-            resolve(image)
-            if (index<(imgArray.length-1)) {
-                getImage(css, path, imgArray, (index+1));
-            }
-        };
-        image.onerror = () => { reject(new Error('Could not load image at ' + url)) };
-        image.src = url;
-    });
-
-}
-
-let addImg = (css, src) => {
-
-    return new Promise((resolve,reject) => {
-        let imgElement = document.createElement("img");
-        imgElement.setAttribute("class", css);
-        imgElement.src = src;
-        document.getElementById("resume").appendChild(imgElement);
-    });
-
+    async function getImage(css, path, imgArray, index) {
+        let image = await loadImage(css, path, imgArray, index);
+        if (index<(imgArray.length-1)){getImage(css, path, imgArray, (index+1))}
+    }
+    function loadImage(css, path, imgArray, index) {
+        let url = path + imgArray[index].image;
+        return new Promise((resolve,reject) => {
+            let image = new Image();
+            image.onload = () => resolve(image);
+            image.onerror = () => { reject(new Error('Could not load image at ' + url)) };
+            image.src = url;
+            let imgElement = document.createElement("img");
+            imgElement.setAttribute("class", css);
+            imgElement.src = url;
+            document.getElementById("resume").appendChild(imgElement);
+        });
+    }
 }
